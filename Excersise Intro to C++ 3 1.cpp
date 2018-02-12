bfb17c5df0d385668ddd28880dc35d5cc4ef9169
Mondotrasho <oscar@peoplepictures.com.au>
2018-02-12 17:12:55 +1100

    boo

---
+++ Excersise Intro to C++ 3 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() //begining bit
+{
+
+	std::cout << "Hello" << std::endl << "World" << std::endl;
+
+	std::cout << "Nectarine" << std::endl;
+	std::cout << "Plum" << std::endl;
+	std::cout << "Grapefruit" << std::endl;
+
+	system("pause"); //Stop terminal from closing at end
+
+	return 0; //end bit
+}
